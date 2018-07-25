3a1a669e21162c243fe390d6113f3d8a47afc0dd
Pedro <ams.pedro@gmail.com>
2018-07-25 22:11:42 +0100

    made word source a bit more interesting

--- words_source.py
+++ words_source.py
@@ -1,3 +1,16 @@
+from random import randint
+
+
 class WordsSource(object):
+    WORD_LIST = [
+        'potato',
+        'pizza',
+        'pasta',
+        'pomegranate',
+        'apple'
+    ]
+
     def random_word(self):
-        return 'potato'
+        return self.WORD_LIST[
+            randint(0, len(self.WORD_LIST))
+        ]
